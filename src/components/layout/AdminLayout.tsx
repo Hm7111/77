42fe9b980ada953b343760b68c947b52f9169530
Hm7111 <hmd79770@gmail.com>
2025-05-26 18:58:37 +0300

    Fix mismatched JSX tags in AdminLayout

--- src/components/layout/AdminLayout.tsx
+++ src/components/layout/AdminLayout.tsx
@@ -12,7 +12,7 @@ export function AdminLayout() {
           <main className="flex-1 p-6 transition-colors duration-300 bg-gray-100 dark:bg-gray-900 overflow-y-auto">
             <Outlet />
           </main>
-        </main>
+        </div>
     </div>
   )
 }
